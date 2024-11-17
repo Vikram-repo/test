f756266ba96ef9f74eea995288712ca925b964df
vikram <vikramazure02@gmail.com>
2024-11-17 16:37:55 +0530

    2nd storage account add

--- main.tf
+++ main.tf
@@ -1,16 +1,24 @@
 resource "azurerm_resource_group" "rg-b" {
-    name = "testrg"
-    location = "centralindia"
-  
+  name     = "testrg"
+  location = "centralindia"
+
 }
 
 
 
 resource "azurerm_storage_account" "stg-b" {
-  name = "test001stg"
-  location = "centralindia"
-  resource_group_name = "testrg"
-  account_tier = "Standard"
+  name                     = "test001stg"
+  location                 = "centralindia"
+  resource_group_name      = "testrg"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_account" "stg-b1" {
+  name                     = "test002stg"
+  location                 = "centralindia"
+  resource_group_name      = "testrg"
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+
+}
