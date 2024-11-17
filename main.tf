d3713daa1994031028680b2aad729d879d9cdc50
vikram <vikramazure02@gmail.com>
2024-11-17 16:04:23 +0530

    this is my storage code for testing

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "rg-b" {
+    name = "testrg"
+    location = "centralindia"
+  
+}
+
+
+
+resource "azurerm_storage_account" "stg-b" {
+  name = "test001stg"
+  location = "centralindia"
+  resource_group_name = "testrg"
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+
